bca71e3dc029560ed87db6619420845687679fc8
Maximilian Svensson <maximilian.svensso1@learnet.se>
2021-11-17 10:32:42 +0100

    Backend

--- src/services/CalenderService.js
+++ src/services/CalenderService.js
@@ -2,11 +2,17 @@ const {google} = require('googleapis');
 const axios = require("axios")
 
 let UserService = require("./UserService");
+const { OAuth2Client } = require('googleapis-common');
 UserService = new UserService();
 
 class CalenderService {
     async getWeekEvents(id) {
         try {
+            OAuthClient = new google.auth.OAuth2();
+
+            OAuth2Client.setCredentials({
+                access_token: user.accessToken
+            })
             const user = await this.getUser(id)
             const calender = google.calendar({
                 auth: user.accessToken,
