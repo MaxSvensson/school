a7021b8513b1f51276d11e0df3f218320493b815
Maximilian Svensson <Max.svensson@outlook.com>
2022-04-04 10:06:50 +0200

    need new ui

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const { route } = require("./user");
 const { getAllWeekEvents, getCoursesId, getCourseWork } = require("../OAuth");
-const router = express.Router()
+const router = express.Router();
 
 router.get("/", async (req, res) => {
-    console.log(await getCourseWork(req.user))
-    res.render("home")
-})
+  console.log(await getCourseWork(req.user));
+  res.render("home");
+});
 
 router.get("/settings", function (req, res) {
-    res.render('settings')
-})
+  res.render("settings");
+});
 
 router.get("/register", (req, res) => {
-    res.render("register")
-})
+  res.render("register");
+});
 router.get("/contact", function (req, res) {
-    res.render('contact')
-})
+  res.render("contact");
+});
 
 module.exports = router;
--- src/services/CalenderService.js
+++ src/services/CalenderService.js
@@ -1,33 +1,32 @@
-const {google} = require('googleapis'); 
-const axios = require("axios")
+const { google } = require("googleapis");
+const axios = require("axios");
 
 let UserService = require("./UserService");
-const { OAuth2Client } = require('googleapis-common');
+const { OAuth2Client } = require("googleapis-common");
 UserService = new UserService();
 
 class CalenderService {
-    async getWeekEvents(id) {
-        try {
-            OAuthClient = new google.auth.OAuth2();
+  async getWeekEvents(id) {
+    try {
+      OAuthClient = new google.auth.OAuth2();
 
-            OAuth2Client.setCredentials({
-                access_token: user.accessToken
-            })
-            const user = await this.getUser(id)
-            const calender = google.calendar({
-                auth: user.accessToken,
-                version: 'v3'
-            })
+      OAuth2Client.setCredentials({
+        access_token: user.accessToken,
+      });
+      const user = await this.getUser(id);
+      const calender = google.calendar({
+        auth: user.accessToken,
+        version: "v3",
+      });
 
-            const response = await axios.get(`https://www.googleapis.com/calendar/v3/users/me/calendarList?accesstoken=${user.accessToken}`)
-            console.log(response)
+      const response = await axios.get(
+        `https://www.googleapis.com/calendar/v3/users/me/calendarList?accesstoken=${user.accessToken}`
+      );
+      console.log(response);
+    } catch (error) {}
+  }
 
-        } catch (error) {
-            
-        }
-
-    }
+  async postEventToCalender() {}
 }
-
 
 module.exports = CalenderService;
